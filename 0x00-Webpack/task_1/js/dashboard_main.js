08e7a77e66d50aea3ce6e7755d411a86262ae49b
Ken Kamau <20206809+Ken-Kamau@users.noreply.github.com>
2023-05-16 10:17:19 +0800

    Update dashboard_main.js

--- 0x00-Webpack/task_1/js/dashboard_main.js
+++ 0x00-Webpack/task_1/js/dashboard_main.js
@@ -1,28 +1,17 @@
 import $ from "jquery";
-import debounce from "lodash/debounce";
+import _ from "lodash";
 
-// Create and append the paragraph elements
 $("body").append("<p>Holberton Dashboard</p>");
 $("body").append("<p>Dashboard data for the students</p>");
-
-// Create and append the button element
-const button = $("<button></button>").text("Click here to get started");
-$("body").append(button);
-
-// Create and append the paragraph element with id 'count'
-const countParagraph = $("<p></p>").attr("id", "count");
-$("body").append(countParagraph);
-
-// Create and append the last paragraph element
+$("body").append("<button>Click here to get started</button>");
+$("body").append('<p id="count"></p>');
 $("body").append("<p>Copyright - Holberton School</p>");
 
 let count = 0;
 
-// Function to update the counter and set the content of the count paragraph
-const updateCounter = () => {
+function updateCounter() {
   count++;
-  countParagraph.text(`${count} clicks on the button`);
-};
+  $("#count").html(`${count} clicks on the button`);
+}
 
-// Bind debounce function to the button's click event
-button.on("click", debounce(updateCounter, 500));
+$("button").on("click", _.debounce(updateCounter, 500));
